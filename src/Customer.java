2d99e44d087c9131924fb7262293e143f110a2ed
schmida736 <schmidtd.mail@gmail.com>
2021-05-11 11:31:58 +0200

    Refactoring: small changes
    Deleted unused imports, added default statement to switch etc...

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,3 @@
-import java.lang.*;
 import java.util.*;
 
 class Customer {
@@ -43,19 +42,18 @@ class Customer {
     private double amountFor(Rental each) {
         double thisAmount = 0;
         switch (each.getMovie().getPriceCode()) {
-            case Movie.REGULAR:
+            case Movie.REGULAR -> {
                 thisAmount += 2;
                 if (each.getDaysRented() > 2)
                     thisAmount += (each.getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
-                break;
-            case Movie.CHILDRENS:
+            }
+            case Movie.NEW_RELEASE -> thisAmount += each.getDaysRented() * 3;
+            case Movie.CHILDRENS -> {
                 thisAmount += 1.5;
                 if (each.getDaysRented() > 3)
                     thisAmount += (each.getDaysRented() - 3) * 1.5;
-                break;
+            }
+            default -> { return -1; }
         }
         return thisAmount;
     }
